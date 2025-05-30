d629d6abac0a2f37a5ef7a940f354591dedc6259
Davi5266 <davi.souza09526@gmail.com>
2025-05-29 22:31:52 -0300

    Docker + Postgres + FastApi

--- app/api/deps.py
+++ app/api/deps.py
@@ -14,7 +14,7 @@ from core.security import SECRET_KEY, ALGORITHM
 #     finally:
 #         db.close()
 
-
+# Recebe o JWT extraido do oauth2_scheme, através de uma dependencia(Depends) que tem como parâmetro o oauth2_scheme que retorna o token extraido da requisição 
 def get_current_client(token:str = Depends(oauth2_scheme),db: Session = Depends(get_db)):
 
     # Resposta para tratar errors de autorização de rota
@@ -26,17 +26,19 @@ def get_current_client(token:str = Depends(oauth2_scheme),db: Session = Depends(
 
     
     try:
+        # extraindo credenciais do token
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        
+        # pegando o parâmetro name extraido do token
         name: str = payload.get("sub")
-        
+        # Se o name não existe retorna um erro de credenciais
         if name is None:
             raise credentials_exception
-    except JWTError: 
+    except JWTError: # Se ouver algum erro durante a extração das credenciais do token entra em uma exceção JWTError e retorna um erro de credencias
         raise credentials_exception
 
+    # Realiza uma comparação do name extraido do token com o name do banco de dados
     client = get_name_by_client(db, name)
-    
+    # se o client for vazio retorna um erro de credenciais
     if client is None:
         raise credentials_exception
     
--- app/core/security.py
+++ app/core/security.py
@@ -9,10 +9,13 @@ from api.deps import get_db
 # config bcrypt
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+# Recebe uma senha digitada pelo usuário e a criptografa
 def hash_password(password:str) -> str:
     return pwd_context.hash(password)
 
+# Realiza uma comparação entre a senha digitada com a senha registrada no sistema
 def verify_password(plain_password:str, hashed_password:str) -> bool:
+    # A senha digitada é criptografada e o seu hash é comparado com o hash no sistema
     return pwd_context.verify(plain_password, hashed_password)
 
 # config JWT
@@ -20,20 +23,25 @@ SECRET_KEY = "" # signature token
 ALGORITHM = "HS256" # hash type
 ACCESS_TOKEN_EXPIRE_MINUTES = 30 # Validation token
 
+# a função create_access_token gera um token com base em dois parêmtros, data que armazena infomações do usuário, e o expires_delta que define um tempo de expiração opcional
 def create_access_token(data:dict, expires_delta:timedelta | None = None):
 
-    to_encode = data.copy()
-    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode = data.copy() # cópia dos dados gerados pelo usuário
+    expire = datetime.utcnow() + (expires_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)) # Define o tempo de vida do JWT, se o expires_delta for None(vazio) o tempo padrão vai ser de 15 minutos 
 
-    to_encode.update({"exp":expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    to_encode.update({"exp":expire}) # passa o tempo de expiração do JWT como parâmetro do dicionario data
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM) # codifica o token JWT com algoritmo HS256 e uma chave secreta
     return encoded_jwt
 
+# Verificador de token
 def verify_token(token:str):
+    # Recebe o token do usuário como parâmetro, e tenta decodificar caso de error a função entra na exceção JWTError e retorna vazio(None)
     try:
+        # Decodificando o token atribuido e utilizando como parâmetro a chave secreta(SECRET_KEY) e o algoritmo(ALGORITHM)
         payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
         return payload
     except JWTError:
         return None
     
+# Dependencia que extrai o token JWT do cabeçalho {Authorization: Bearer <token>}, e o parâmetro tokenUrl passa o endpoint reponsavel por fornecer o token, através de uma rota POST /login que passa um body com username e password (no caso desse código ele passa um body com email e password)
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="client/login")
